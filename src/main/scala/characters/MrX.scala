318a81a59c1640529c35da772bd0fb1e95186e3d
Bernhard Fraidel <bernhardfraidel@gmail.com>
2023-01-21 11:59:23 +0100

    add maps for bus tax and underground

--- src/main/scala/characters/MrX.scala
+++ src/main/scala/characters/MrX.scala
@@ -1,10 +1,11 @@
 package characters
 
 class MrX(name: String, currentLocation: Int) extends PlayerCharacter(name, currentLocation) {
+  var blackTickets = 0
+  var doubleTickets = 2
   taxiTickets = 4
   busTickets = 3
   subwayTickets = 3
-  doubleTickets = 2
 
   def setBlackTickets(numDetectives: Int): Unit =
     blackTickets = numDetectives
